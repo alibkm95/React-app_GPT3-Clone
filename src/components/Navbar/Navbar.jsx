5bfd3baed1305ff38f31f05758f067b2164490c7
alibkm95 <ali.bkm95@gmail.com>
2024-03-30 19:35:33 +0330

    asset fies added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='gp3__navbar'>
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="LOGO" />
+        </div>
+      </div>
+    </div>
   )
 }
 
