577c5b0295f5fb87fe66981e86d7a89a4a4beea5
alibkm95 <ali.bkm95@gmail.com>
2024-03-31 13:14:41 +0330

    whatgpt3 component crated and its styles applied

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -5,7 +5,7 @@ import ai from '../../assets/ai.png'
 
 const Header = () => {
   return (
-    <div className='gpt3__header section__padding' id='Home'>
+    <div className='gpt3__header section__padding' id='home'>
       <div className="gpt3__header-content">
         <h1 className="gradient__text">
           Let's Build Something amazing with GPT3 OpenAI
